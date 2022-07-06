febba275c28724fddaf7364b64e959e080ef9375
Mike Bulanov <mikki.messer@gmail.com>
2022-07-06 19:32:35 +0300

    sample totally different test added

--- src/test/java/AllureExampleTests.java
+++ src/test/java/AllureExampleTests.java
@@ -57,7 +57,7 @@ public class AllureExampleTests {
     @Test
     @DisplayName("Open totally different link")
     @Story("Main menu")
-    @Owner("Mike B")
+    @Owner("Mike Bbb")
     @Feature("Main page")
     void totallyDifferentLink() {
         step("Open main page");
